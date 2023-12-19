ca0341f1a7b90da53267946642226cecdda82feb
hachino.Y <149479436+hachinoY@users.noreply.github.com>
2023-12-19 06:57:22 +0000

    [add]rails sの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # top が / の表示になる
   root to: 'homes#top'
-  gets '/homes/about' => 'homes#about' , as: 'about'
+  get '/homes/about' => 'homes#about' , as: 'about'
   # rubyが勝手にroutesを作ってくれる
   # resources :
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
